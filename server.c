858f5e548ac833466477fa105abd1a3f1855dcbc
Caleb Yates <caldog20@gmail.com>
2022-04-06 01:02:05 -0500

    Changed stuff to encode bytes and changed callbacks

--- server.c
+++ server.c
@@ -1,190 +1,171 @@
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <netinet/in.h>
-#include <unistd.h>
+/* This is a simple TCP server that listens on port 1234 and provides lists
+ * of files to clients, using a protocol defined in file_server.proto.
+ *
+ * It directly deserializes and serializes messages from network, minimizing
+ * memory use.
+ *
+ * For flexibility, this example is implemented using posix api.
+ * In a real embedded system you would typically use some other kind of
+ * a communication and filesystem layer.
+ */
+
 #include <dirent.h>
+#include <netinet/in.h>
+#include <sys/ioctl.h>
+#include <arpa/inet.h>
 #include <stdio.h>
 #include <string.h>
-#include <arpa/inet.h>
-#include <pb_encode.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <pb_decode.h>
-#include <sys/ioctl.h>
-#include "siopayload.pb.h"
-#include "common.h"
+#include <pb_encode.h>
 
+#include "common.h"
+#include "siopayload.pb.h"
+#include <pb_decode.h>
+#include <pb_common.h>
 #define HOST_IP "0.0.0.0"
 #define HOST_PORT 3333
 
-/* This callback function will be called during the encoding.
- * It will write out any number of FileInfo entries, without consuming unnecessary memory.
- * This is accomplished by fetching the filenames one at a time and encoding them
- * immediately.
+typedef struct _mydata_t 
+{ 
+size_t len; 
+uint8_t *buf; 
+} mydata_t; 
+
+bool encode_string(pb_ostream_t *stream, const pb_field_t *field,
+                   void *const *arg) {
+  const char *str = "mystring";
+
+  if (!pb_encode_tag_for_field(stream, field))
+    return false;
+
+  return pb_encode_string(stream, (const uint8_t *)str, strlen(str));
+}
+
+bool encode_bytes(pb_ostream_t *stream, const pb_field_t *field,
+                   void *const *arg) {
+    mydata_t *data = *arg;
+
+  if (!pb_encode_tag_for_field(stream, field))
+    return false;
+
+  return pb_encode_string(stream, data->buf, data->len);
+}
+
+bool DataTransfer_callback(pb_istream_t *stream, const pb_field_t *field, void **arg) {
+    
+    uint8_t buffer[1024];
+    
+    int strlen = stream->bytes_left;
+    mydata_t data = {strlen, buffer};
+    if (strlen > sizeof(buffer) - 1)
+      return false;
+
+    if (!pb_read(stream, buffer, strlen))
+        return false;
+    buffer[strlen] = '\0';
+    /*printf("size was %d and bytes were %s\n", strlen, data.buf);*/
+    printf("%s\n", data.buf);
+    return true;
+
+}
+
+/* The callback below is a message-level callback which is called before each
+ * submessage is decoded. It is used to set the pb_callback_t callbacks inside
+ * the submessage. The reason we need this is that different submessages share
+ * storage inside oneof union, and before we know the message type we can't set
+ * the callbacks without overwriting each other.
  */
-// bool ListFilesResponse_callback(pb_istream_t *istream, pb_ostream_t *ostream, const pb_field_iter_t *field)
-// {
-//     PB_UNUSED(istream);
-//     if (ostream != NULL && field->tag == ListFilesResponse_file_tag)
-//     {
-//         DIR *dir = *(DIR**)field->pData;
-//         struct dirent *file;
-//         FileInfo fileinfo = {};
+bool msg_callback(pb_istream_t *stream, const pb_field_t *field, void **arg) {
+  /* Print the prefix field before the submessages.
+   * This also demonstrates how to access the top level message fields
+   * from callbacks.
+   */
 
-//         while ((file = readdir(dir)) != NULL)
-//         {
-//             fileinfo.inode = file->d_ino;
-//             strncpy(fileinfo.name, file->d_name, sizeof(fileinfo.name));
-//             fileinfo.name[sizeof(fileinfo.name) - 1] = '\0';
+  if (field->tag == SIOPayload_data_transfer_tag) {
+      DataTransfer *msg = field->pData;
+    /*printf("bytes message\n");*/
+      msg->data.funcs.decode = DataTransfer_callback;
+  } else if (field->tag == SIOPayload_flow_control_tag) {
+    printf("Flow Control:\n");
+  }
 
-//             /* This encodes the header for the field, based on the constant info
-//             * from pb_field_t. */
-//             if (!pb_encode_tag_for_field(ostream, field))
-//                 return false;
+  return true;
+}
 
-//             /* This encodes the data for the field, based on our FileInfo structure. */
-//             if (!pb_encode_submessage(ostream, FileInfo_fields, &fileinfo))
-//                 return false;
-//         }
 
-//         /* Because the main program uses pb_encode_delimited(), this callback will be
-//         * called twice. Rewind the directory for the next call. */
-//         rewinddir(dir);
-//     }
 
-//     return true;
-// }
+void handle_connection(int connfd, int argc, char** argv) {
 
-/* Handle one arriving client connection.
- * Clients are expected to send a ListFilesRequest, terminated by a '0'.
- * Server will respond with a ListFilesResponse message.
- */
-void handle_connection(int connfd)
-{
-    int pin1, pin2;
+
     while(1) {
         int count = 0;
         ioctl(connfd, FIONREAD, &count);
-        // If we received a message, send one back to toggle PSX lines
-            if (count > 0){
-            /* Decode the message from the client and open the requested directory. */
-                SIOPayload payload = {};
-                pb_istream_t input = pb_istream_from_socket(connfd);
-                
-                if (!pb_decode_delimited(&input, SIOPayload_fields, &payload))
-                {
-                    printf("Decode failed: %s\n", PB_GET_ERROR(&input));
-                    return;
-                }
-                
-                /* Print the data contained in the message. */
-                printf("SIO Payload:\n");
-                printf("which_type: %d\n", payload.which_type);
-                switch(payload.which_type)
-                {
-                    case SIOPayload_data_transfer_tag:
-                        printf("Data Transfer Message\n");
-                        // printf("  data_send.length: %d\n", payload.type.data_transfer);
-                        break;
+        if (count > 0) {
 
-                    case SIOPayload_flow_control_tag:
-                        printf("Flow Control Message\n");
-                        printf("psx_rts: %d\n", payload.type.flow_control.dxr);
-                        printf("psx_dtr: %d\n\n", payload.type.flow_control.xts);
-                        pin1 = payload.type.flow_control.dxr;
-                        pin2 = payload.type.flow_control.xts;
-                        break;
-                }
-            {
-                printf("Sending a response\n");
-                FlowControl ftcl = FlowControl_init_zero;
-                ftcl.dxr = pin1;
-                ftcl.xts = pin2;
-                SIOPayload payload = SIOPayload_init_zero;
-                pb_ostream_t output = pb_ostream_from_socket(connfd);
-                payload.type.flow_control = ftcl;
-                payload.which_type = SIOPayload_flow_control_tag;
-                printf("Encoding\n");
-                if (!pb_encode_delimited(&output, SIOPayload_fields, &payload)) {
-                    printf("Encoding failed: %s\n", PB_GET_ERROR(&output));
-                    break;
-                }
-                printf("Sent\n");
-            }
+          SIOPayload msg = SIOPayload_init_zero;
+          msg.cb_type.funcs.decode = msg_callback;
+          pb_istream_t stream = pb_istream_from_socket(connfd);
+          if (!pb_decode_delimited(&stream, SIOPayload_fields, &msg)) {
+            fprintf(stderr, "Decoding failed: %s\n", PB_GET_ERROR(&stream));
+          }
+          
+          if (msg.which_type == SIOPayload_flow_control_tag ) {
+            printf("dxr: %d, xts: %d\n", msg.type.flow_control.dxr, msg.type.flow_control.xts);
+          } 
+
         }
+    }
+}
 
 
 
-}
-    // /* List the files in the directory and transmit the response to client */
-    // {
-    //     ListFilesResponse response = {};
-    //     pb_ostream_t output = pb_ostream_from_socket(connfd);
-        
-    //     if (directory == NULL)
-    //     {
-    //         perror("opendir");
-            
-    //         /* Directory was not found, transmit error status */
-    //         response.has_path_error = true;
-    //         response.path_error = true;
-    //     }
-    //     else
-    //     {
-    //         /* Directory was found, transmit filenames */
-    //         response.has_path_error = false;
-    //         response.file = directory;
-    //     }
-        
-    //     if (!pb_encode_delimited(&output, ListFilesResponse_fields, &response))
-    //     {
-    //         printf("Encoding failed: %s\n", PB_GET_ERROR(&output));
-    //     }
-    // }
-}
-int main(int argc, char **argv)
-{
-    int listenfd, connfd;
-    struct sockaddr_in servaddr;
-    int reuse = 1;
-    
-    /* Listen on localhost:1234 for TCP connections */
-    listenfd = socket(AF_INET, SOCK_STREAM, 0);
-    setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse));
-    
-    memset(&servaddr, 0, sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
-    inet_pton(AF_INET, HOST_IP, &servaddr.sin_addr); 
-    servaddr.sin_port = htons(HOST_PORT);
-    if (bind(listenfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) != 0)
-    {
-        perror("bind");
-        return 1;
+int main(int argc, char **argv) {
+
+  int listenfd, connfd;
+  struct sockaddr_in servaddr;
+  int reuse = 1;
+
+  /* Listen on localhost:1234 for TCP connections */
+  listenfd = socket(AF_INET, SOCK_STREAM, 0);
+  setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse));
+
+  memset(&servaddr, 0, sizeof(servaddr));
+  servaddr.sin_family = AF_INET;
+  inet_pton(AF_INET, HOST_IP, &servaddr.sin_addr); 
+  
+  servaddr.sin_port = htons(HOST_PORT);
+  if (bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) != 0) {
+    perror("bind");
+    return 1;
+  }
+
+  if (listen(listenfd, 5) != 0) {
+    perror("listen");
+    return 1;
+  }
+
+  for (;;) {
+    /* Wait for a client */
+    connfd = accept(listenfd, NULL, NULL);
+
+    if (connfd < 0) {
+      perror("accept");
+      return 1;
     }
-    
-    if (listen(listenfd, 5) != 0)
-    {
-        perror("listen");
-        return 1;
-    }
-    
-    for(;;)
-    {
-        /* Wait for a client */
-        connfd = accept(listenfd, NULL, NULL);
-        
-        if (connfd < 0)
-        {
-            perror("accept");
-            return 1;
-        }
-        
-        printf("Got connection.\n");
-        
-        handle_connection(connfd);
-        
-        printf("Closing connection.\n");
-        
-        // close(connfd);
-    }
-    
-    return 0;
+
+    printf("Got connection.\n");
+
+    handle_connection(connfd, argc, argv);
+
+    /* printf("Closing connection.\n"); */
+
+    /* close(connfd); */
+  }
+  return 0;
 }
+
